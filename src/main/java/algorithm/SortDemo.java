2739c6c5cefbc4a83ff5d7c7526a3f2d696bbbe8
didi <gaojianxin@didiglobal.com>
2020-10-10 15:13:26 +0800

    增加 2.选择排序

--- src/main/java/algorithm/SortDemo.java
+++ src/main/java/algorithm/SortDemo.java
@@ -11,14 +11,16 @@ public class SortDemo {
 
     public static void main(String[] args) {
 
-        int[] arrayList = new int[]{6,2,3,1};
+        int[] arrayList = new int[]{6,2,3,3,2,1};
         System.out.println(arrayList.length);
         arrayToString(arrayList);
-        arrayToString(bubbleSort(arrayList));
+        //arrayToString(bubbleSort(arrayList));
+        arrayToString(selectionSort(arrayList));
+
     }
 
     /**
-     * 冒泡排序：相邻两个元素比较大小，最后最大的数据冒出，一趟完成，然后循环进行
+     * 1.冒泡排序：相邻两个元素比较大小，最后最大的数据冒出，一趟完成，然后循环进行
      * @param array
      * @return
      */
@@ -32,6 +34,31 @@ public class SortDemo {
                     array[j+1] = temp;
                 }
             }
+        }
+        return array;
+    }
+
+
+    /**
+     * 2.选择排序：在未排序序列中，找到最大的数据，放在第1位，再从剩余中未排序序列中找到最大数据，依次排列
+     * @param array
+     * @return
+     */
+    public  static int[] selectionSort(int[] array){
+        int maxIndex=0;
+        for(int i =0; i<array.length-1; i++){
+            maxIndex = i;
+            //选择排序第二个循环是从i+1开始
+            for(int j =i+1; j<array.length; j++){
+                if(array[maxIndex] < array[j]){
+                    maxIndex = j;
+                }
+            }
+            //每次找出未排序最大的值，就与最左端互换位置，这样遍历就是每次选择出最大的元素了
+            int temp = array[i];
+            array[i] = array[maxIndex];
+            array[maxIndex] = temp;
+
         }
         return array;
     }
